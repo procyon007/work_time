cd5b23e175c866274883a7323b6de8834a1a6966
procyon007 <ta.uta07@gmail.com>
2020-01-07 08:56:32 -0500

    update

--- test.py
+++ test.py
@@ -6,5 +6,5 @@ end_date = datetime.datetime(2020,1,31)
 wd = workdays.networkdays(start_date, end_date)
 work_time = int(wd) * 7.5
 ov_time = int(input("想定する残業時間を入力してください: "))
-total_time = 
-print(wd)
+total_time = work_time + ov_time
+print("総稼働時間: " + str(total_time) + "H(内残業" + str(ov_time) + "H)" )
